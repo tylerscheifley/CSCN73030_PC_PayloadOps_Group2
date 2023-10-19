d320f10be4f56a7e7df0ee03a171a288c6e73302
Owen Garland <Ogarland9238@conestogac.on.ca>
2023-10-19 18:34:58 -0400

    first server test commit with stub server object

--- server/index.js
+++ server/index.js
@@ -5,9 +5,18 @@ app.use(express.static("client/build"));
 // let the react app to handle any unknown routes
 // serve up the index.html if express does'nt recognize the route
 const path = require("path");
-app.get("*", (req, res) => {
+//Default route
+app.get("/", (req, res) => {
   res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
 });
+
+//Get Image from payload
+app.get("/ImagePayloadRequest", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "sclient", "build", "index.html"));
+});
+
+//
+
 // if not in production use the port 5000
 const PORT = process.env.PORT || 5000;
 console.log("server started on port:", PORT);
--- server/server.test.js
+++ server/server.test.js
@@ -1,5 +1,12 @@
-const sum = require('./sum');
+const request = require("supertest");
+const server = require("../server/serverStubFunctions");
 
-test('adds 1 + 2 to equal 3', () => {
-  expect(sum(1, 2)).toBe(3);
+describe("GET /GroundStationPayload", () => {
+  it("should return a message and have a length greater than 0", async () => {
+    const response = await request(server).get("/GroundStationPayload");
+    expect(response.statusCode).toBe(200);
+    expect(response.body.message).toEqual("Ground Station Payload");
+  });
 });
+
+server.close();
