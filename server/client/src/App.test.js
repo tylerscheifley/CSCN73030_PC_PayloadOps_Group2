691dc84f327b48feb0ced14355e9a5a73c298aa2
RyanCoding19 <Ryan_Wallace2019@outlook.com>
2023-10-19 19:36:57 -0400

    set up multiple single text checking Unit Tests

--- server/client/src/App.test.js
+++ server/client/src/App.test.js
@@ -1,5 +1,6 @@
-import { render, screen } from '@testing-library/react';
+import { getAllByText, queryAllByText, render, screen } from '@testing-library/react';
 import App from './App';
+
 
 test('renders learn react link', () => {
   render(<App />);
@@ -7,3 +8,44 @@ test('renders learn react link', () => {
   expect(linkElement).toBeInTheDocument();
 });
 
+test("Checking text waiting for satellite Imagery", () => {
+render(<App />);
+const { getByText } = render(<h1 style={{  paddingLeft: 30}}>PC-Payload Ops</h1>);
+const PayloadElement = getByText(/📷 Waiting for satellite imagery.../i);
+expect(PayloadElement).toBeInTheDocument();
+});
+
+test("Checking text waiting for Waterloo Ontario", () => {
+  render(<App />);
+  const { getByText } = render(<h1 style={{  paddingLeft: 30}}>PC-Payload Ops</h1>);
+  const PayloadElement = getByText(/🍁Waterloo Ontario/i);
+  expect(PayloadElement).toBeInTheDocument();
+  });
+
+  test("Checking text waiting for Submit Text", () => {
+    render(<App />);
+    const { getByText } = render(<h1 style={{  paddingLeft: 30}}>PC-Payload Ops</h1>);
+    const PayloadElement = getByText(/Submit/i);
+    expect(PayloadElement).toBeInTheDocument();
+    });
+
+    test("Checking text waiting for Coordiantes", () => {
+      render(<App />);
+      const { getByText } = render(<h1 style={{  paddingLeft: 30}}>PC-Payload Ops</h1>);
+      const PayloadElement = getByText(/10:22:01-2023-10-18/i);
+      expect(PayloadElement).toBeInTheDocument();
+      });
+
+      test("Checking text waiting for Time", () => {
+        render(<App />);
+        const { getByText } = render(<h1 style={{  paddingLeft: 30}}>PC-Payload Ops</h1>);
+        const PayloadElement = getByText(/10:22:01-2023-10-18/i);
+        expect(PayloadElement).toBeInTheDocument();
+        });
+
+        test("Checking text waiting for ID", () => {
+          render(<App />);
+          const { getByText } = render(<h1 style={{  paddingLeft: 30}}>PC-Payload Ops</h1>);
+          const PayloadElement = getByText(/I1/i);
+          expect(PayloadElement).toBeInTheDocument();
+          });
