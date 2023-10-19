ca70e6212a26a34a0ea8d802e24cd45f4bf6380c
Brodin Collins-Robb <tapeit03@outlook.com>
2023-10-18 22:30:19 -0400

    Setup a table in the app

--- server/client/src/App.js
+++ server/client/src/App.js
@@ -1,6 +1,13 @@
 import logo from "./Earth.gif";
 import "./App.css";
 
+
+const data = [
+  { time: "10:22:01-2023-10-18", coordinates:  "41.40338, 2.17403", imageID: "I1" },
+  { time: "10:24:11-2023-10-18", coordinates: "41.40338, 2.17403", imageID: "I2" },
+  { time: "10:34:11-2023-10-18", coordinates: "41.40338, 2.17403", imageID: "I3" },
+]
+
 function MyButton() {
   return (
     <button >I'm a button</button>
@@ -10,16 +17,49 @@ function MyButton() {
 function App() {
   return (
     <div className="App">
+     
       <header className="App-header">
-        <img style={{ float: "right", padding: 20}} src={logo} className="App-logo" alt="logo" width={250} height={250}/>
-        <h1 style={{ paddingTop: 80, paddingLeft: 30}}>PC-Payload Ops</h1>
-        <p style={{ padding: 30}}>
-        🍁Waterloo Ontario
-        </p>
+
+              <h1 style={{  paddingLeft: 30}}>PC-Payload Ops</h1>
+              <p style={{ padding: 30}}>
+              🍁Waterloo Ontario
+              </p>
+         
+      </header>
+
         <div>
+
+            <img style={{ float: "right", padding: 20}} src={logo} className="App-logo" alt="logo" width={600} height={600}/>
+
+            <div className="Table">
+              <table style={{ float: "right"}}>
+                <tr>
+                    <th>Image ID</th>
+                    <th>Time</th>
+                    <th>Coordinates</th>
+                </tr>
+                {data.map((val, key) => {
+                    return (
+                        <tr key={key}>
+                            <td>{val.imageID}</td>
+                            <td>{val.time}</td>
+                            <td>{val.coordinates}</td>
+                        </tr>
+                    )
+                })}
+              </table>
+            </div>
+            
         <MyButton />
         </div>
-      </header>
+
+
+        
+        
+        <div>
+        
+      </div>
+      
     </div>
   );
 }
